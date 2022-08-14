4ea9083b7be4a1a66fad3b17a8aff6723d1bc29d
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-08-14 20:24:51 -0300

    feat: add addresses list style

--- src/components/AddressesList.vue
+++ src/components/AddressesList.vue
@@ -1,36 +1,36 @@
 <template>
-  <section>
-    <table>
-      <tr>
-        <th>CEP</th>
-        <th>Logradouro</th>
-        <th>Bairro</th>
-        <th>Cidade</th>
-        <th>Estado</th>
-      </tr>
-      <tr v-for="address in addressesList" :key="address.cep">
-        <template v-if="editingCep === address.cep">
-          <td>{{ editingCep }}</td>
-          <td><input type="text" v-model="editing.street"/></td>
-          <td><input type="text" v-model="editing.district"/></td>
-          <td><input type="text" v-model="editing.city"/></td>
-          <td><input type="text" v-model="editing.state"/></td>
+  <table>
+    <tr>
+      <th>CEP</th>
+      <th>Logradouro</th>
+      <th>Bairro</th>
+      <th>Cidade</th>
+      <th>Estado</th>
+    </tr>
+    <tr v-for="address in addressesList" :key="address.cep">
+      <template v-if="editingCep === address.cep">
+        <td>{{ editingCep }}</td>
+        <td><input type="text" v-model="editing.street"/></td>
+        <td><input type="text" v-model="editing.district"/></td>
+        <td><input type="text" v-model="editing.city"/></td>
+        <td><input type="text" v-model="editing.state"/></td>
 
-          <button @click="handleUpdate(editingCep)">Salvar</button>
-          <button @click="editingCep = ''">Cancelar</button>
-        </template>
-        <template v-else>
-          <td>{{ address.cep }}</td>
-          <td>{{ address.street }}</td>
-          <td>{{ address.district }}</td>
-          <td>{{ address.city }}</td>
-          <td>{{ address.state }}</td>
-          <button @click="handleUpdating(address.cep)">Editar</button>
-          <button @click="handleDelete(address.cep)">Deletar</button>
-        </template>
-      </tr>
-    </table>
-  </section>
+        <button class="save-button" @click="handleUpdate(editingCep)">Salvar</button>
+        <button class="delete-button" @click="editingCep = ''">Cancelar</button>
+      </template>
+      <template v-else>
+        <td>{{ address.cep }}</td>
+        <td>{{ address.street }}</td>
+        <td>{{ address.district }}</td>
+        <td>{{ address.city }}</td>
+        <td>{{ address.state }}</td>
+        <td>
+          <button class="edit-button" @click="handleUpdating(address.cep)">Editar</button>
+          <button class="delete-button" @click="handleDelete(address.cep)">Deletar</button>
+        </td>
+      </template>
+    </tr>
+  </table>
 </template>
 
 <script lang='ts'>
@@ -84,3 +84,57 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300;700&display=swap');
+
+table {
+  border-radius: 10px;
+  box-shadow: 0 0 6px rgb(199, 199, 199);
+  font-family: 'Manrope', sans-serif;
+  margin: 1em 0;
+  padding: 2em;
+  text-align: center;
+  width: 100%;
+}
+
+button {
+  border-radius: 5px;
+  border: 1px solid;
+  cursor: pointer;
+  color: #fff;
+  font-weight: 700;
+  margin: 0.5em;
+  padding: 0.5em;
+  transition: 0.5s;
+  width: 10em;
+}
+
+.edit-button {
+  background-color: #ffcc00;
+}
+
+.edit-button:hover {
+  color:#ffcc00;
+  background-color: #fff;
+}
+
+.delete-button {
+  background-color: #cc3300;
+}
+
+.delete-button:hover {
+  color: #cc3300;
+  background-color: #fff;
+}
+
+.save-button {
+  background-color: #339900;
+}
+
+.save-button:hover {
+  color: #339900;
+  background-color: #fff;
+}
+
+</style>
