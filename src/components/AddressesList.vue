3cf4dc150844e219a78f4a2562f7e506c6c2629d
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-08-14 15:11:12 -0300

    feat!: add edit/delete buttons
    only the delete button is functional

--- src/components/AddressesList.vue
+++ src/components/AddressesList.vue
@@ -7,12 +7,14 @@
         <th>Cidade</th>
         <th>Estado</th>
       </tr>
-      <tr v-for="address in addressesList" :key="address['cep']">
-        <td>{{ address['cep'] }}</td>
-        <td>{{ address['street'] }}</td>
-        <td>{{ address['district'] }}</td>
-        <td>{{ address['city'] }}</td>
-        <td>{{ address['state'] }}</td>
+      <tr v-for="address in addressesList" :key="address.cep">
+        <td>{{ address.cep }}</td>
+        <td>{{ address.street }}</td>
+        <td>{{ address.district }}</td>
+        <td>{{ address.city }}</td>
+        <td>{{ address.state }}</td>
+        <button>Editar</button>
+        <button @click="handleDelete(address.cep)">Deletar</button>
       </tr>
     </table>
   </section>
@@ -20,14 +22,22 @@
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
+import IAddress from '../interfaces/IAddress';
 import * as api from '../services/api';
+
+const list: IAddress[] = [];
 
 export default defineComponent({
   name: 'AddressesList',
   data() {
     return {
-      addressesList: [],
+      addressesList: list,
     }
+  },
+  methods: {
+    async handleDelete(cep: string) {
+      await api.deleteAddress(cep);
+    },
   },
   async mounted() {
     this.addressesList = await api.fetchAddresses();
